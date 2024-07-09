54e1acb50f0dbf67613faa2d13487f6e5811ce75
Hernán Ponce de León Aguirre <135001744+HPoncedeLeonAguirre@users.noreply.github.com>
2024-07-09 11:02:47 -0500

    Dependencias, index.js, server.js y package.json start Viva Latam

---
+++ back/src/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = require("./routes/index");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
+server.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
+server.use(
+	cors({
+		origin: "*"
+	})
+);
+
+server.use("/", router);
+
+server.use((err, req, res, next) => {
+	const status = err.status || 500;
+	const message = err.message || err;
+	console.log(err);
+	res.status(status).send(message);
+});
+
+module.exports = { server };
