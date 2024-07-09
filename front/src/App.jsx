b76a6570f2e8a704f04419222274e19816bd419c
Gilberto Díaz <diazp.gilberto@gmail.com>
2024-07-08 20:52:38 -0500

    montando primeros componenentes para QR CODE

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
+import "./App.css";
+import "flowbite";
+import 'flowbite/dist/flowbite.css';
+import { Datepicker } from 'flowbite-react';
 
 function App() {
-  
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello Lubricentro
-    </h1>
+     <h3 className="text-3xl font-bold ">Placa: VEX 45FDS</h3>
+     <Datepicker placeholder="Select date" />
+     <h3 className="text-3xl font-bold ">Categoria:</h3>
+     <h3 className="text-3xl font-bold ">Productos escogidos:</h3>
+     <h3 className="text-3xl font-bold ">Observaciones</h3>
+           
     </>
-  )
+  );
 }
 
-export default App
+export default App;
