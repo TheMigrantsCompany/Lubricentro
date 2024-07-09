826931b64343fa887bf6b0a40b615196de26bd91
ARagustinaramos <agusramos12@gmail.com>
2024-07-09 20:33:29 -0300

    footer

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,21 +3,29 @@ import "flowbite";
 import 'flowbite/dist/flowbite.css';
 import { Datepicker } from 'flowbite-react';
 import ProductCard from "./components/ProductCard";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <>
-     <h3 className="text-3xl font-bold ">Placa: VEX 45FDS</h3>
-     <Datepicker placeholder="Select date" />
-     <h3 className="text-3xl font-bold ">Categoria:</h3>
-     <h3 className="text-3xl font-bold ">Productos escogidos:</h3>
-     <ProductCard />
-     <ProductCard />
-     <ProductCard />
-     <h3 className="text-3xl font-bold ">Observaciones</h3>
-           
-    </>
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <h3 className="text-3xl font-bold">Placa: VEX 45FDS</h3>
+        <Datepicker placeholder="Select date" />
+        <h3 className="text-3xl font-bold">Categoria:</h3>
+        <h3 className="text-3xl font-bold">Productos escogidos:</h3>
+      </header>
+      <main className="flex-grow">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <h3 className="text-3xl font-bold">Observaciones</h3>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
+
+
