88c91b4f30fee57db707edbb17942d82b3cdba5d
Iara Baudino <iaba.sur@gmail.com>
2024-07-10 10:57:59 -0300

    primeras rutas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -12,11 +12,15 @@ import Landing from "./views/landing/Landing";
 function App() {
   return (
     <>
-      
-      
       <Routes>
-        
         <Route path="/" element={<Landing />} />
+        <Route path="/admin/manage_employees" element={ <h1>manage_employees</h1>} />
+        <Route path="/admin/manage_products" element={<h1>manage_products</h1>} />
+        <Route path="/admin/manage_orders" element={<h1>manage_orders</h1>} />
+        <Route path="/employee/services" element={<h1>services</h1>} />
+        <Route path="/employee/create_order" element={<FormOrdenServicio />} />
+        <Route path="/employee/inventary" element={<h1>inventary</h1>} />
+        <Route path="/employee/create_user" element={<h1>create_user</h1>} />
       </Routes>
     </>
   );
