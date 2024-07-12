d5a4aa33f9052af1019eaf0f44deb71f28bfe273
Laura Camila Tiusabá Gómez <108424409+LauraCamila99@users.noreply.github.com>
2024-07-12 18:44:21 -0500

    Modularización para la creacion de CRUDS Y funciones anexas para el modelo de productService

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
 const carRouter = require("./carRouter");
+const productServiceRouter = require("./productServiceRouter");
 const router = Router();
 
+// Agrega las rutas de carro a la ruta principal
 router.use("/cars", carRouter);
+// Agrega las rutas de producto a la ruta principal
+router.use("/products", productServiceRouter);
 
 module.exports = router;
