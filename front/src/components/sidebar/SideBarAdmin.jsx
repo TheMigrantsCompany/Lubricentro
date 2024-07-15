6af21bd9d3afd704d696121fc369f794934241ab
ARagustinaramos <agusramos12@gmail.com>
2024-07-15 13:36:23 -0300

    manage employee, cambio de colores en navbar y en index global

--- front/src/components/sidebar/SideBarAdmin.jsx
+++ front/src/components/sidebar/SideBarAdmin.jsx
@@ -5,12 +5,16 @@ import { HiUser, HiShoppingBag, HiInbox } from 'react-icons/hi';
 const SideBarAdmin = () => {
   return (
     <div className="h-screen">
-      <Sidebar aria-label="Sidebar with logo branding example" className="h-full bg-gray-400">
-        <div className="flex flex-col h-full justify-between pt-24"> 
+      <Sidebar
+        aria-label="Sidebar with logo branding example"
+        className="h-full"
+        style={{ backgroundColor: '#D9D9D9 !important' }}
+      >
+        <div className="flex flex-col h-full justify-between pt-24">
           <Sidebar.Items>
             <Sidebar.ItemGroup className="flex flex-col gap-4">
               <Sidebar.Item href="/admin/manage_employees" icon={HiUser}>
-               Gestion de Empleados
+                Gestion de Empleados
               </Sidebar.Item>
               <Sidebar.Item href="/admin/manage_clients" icon={HiShoppingBag}>
                 Gestion de Clientes
--- front/src/views/manage_employees/Manage_employees.jsx
+++ front/src/views/manage_employees/Manage_employees.jsx
@@ -3,11 +3,13 @@ import SideBarAdmin from '../../components/sidebar/SideBarAdmin';
 import { TableLubricadores } from '../../components/table_lubricadores/Table_Lubricadores';
 import BestLubricador from '../../components/cardstat/CardStat1';
 import CardStat2 from '../../components/cardstat/CardStat2';
+import { EmployeeChart } from '../../components/charts/EmployeeChart'; 
+
 
 const GestionEmpleados = () => {
   return (
     <div className="flex">
-      <SideBarAdmin /> {/* Sidebar en el costado */}
+      <SideBarAdmin /> 
 
       <div className="flex-1 p-4">
         <div className="flex justify-between mb-4">
@@ -15,7 +17,14 @@ const GestionEmpleados = () => {
           <CardStat2 />
         </div>
         
-        <TableLubricadores />
+        <div className="flex">
+          <div className="flex-1">
+            <TableLubricadores />
+          </div>
+          <div className="w-1/3 p-4">
+            <EmployeeChart />
+          </div>
+        </div>
       </div>
     </div>
   );
