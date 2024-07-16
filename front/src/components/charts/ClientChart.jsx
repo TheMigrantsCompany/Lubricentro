14cfa402619d1a35cd07a7957d5c754a7d952b3f
ARagustinaramos <agusramos12@gmail.com>
2024-07-16 11:46:24 -0300

    solucionado problemas con graficos y estilos en admin

--- front/src/components/charts/ClientChart.jsx
+++ front/src/components/charts/ClientChart.jsx
@@ -1,6 +1,6 @@
-'use client';
+'use Client'
 
-import { FunnelChart } from '@tremor/react';
+import { BarChart } from '@tremor/react';
 
 const chartdataClientes = [
   { name: 'Cliente A', value: 120 },
@@ -8,28 +8,33 @@ const chartdataClientes = [
   { name: 'Cliente C', value: 30 },
 ];
 
+const dataFormatter = (number) => Intl.NumberFormat('us').format(number).toString();
+
 export function ClientesChart() {
   return (
     <div className="border border-black rounded-lg p-4 bg-white">
-      <h3 className="text-black text-tremor-default text-tremor-content dark:text-dark-tremor-content text-sm">
+      <h3 className="text-black text-tremor-default dark:text-dark-tremor-content">
         Clientes con Más y Menos Compras
       </h3>
-      <p className="text-black text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content text-sm">
+      <p className="text-black text-tremor-metric font-semibold dark:text-dark-tremor-content ">
         8.3%
       </p>
-      <p className="mt-4 text-black text-tremor-label text-tremor-content dark:text-dark-tremor-content text-sm">
+      <p className="mt-4 text-black text-tremor-label dark:text-dark-tremor-content ">
         Clientes únicos que realizaron compras en los últimos 30 días.
       </p>
       <div className="flex justify-center">
-        <FunnelChart
+        <BarChart
           className="mt-4 w-full max-w-md"
           data={chartdataClientes}
-          style={{
-            label: { fill: 'black', fontSize: 12, fontWeight: 'bold' },
-            tooltip: { background: 'white', border: '1px solid #ccc', color: 'black', fontSize: 12, fontWeight: 'normal' }
-          }}
+          index="name"
+          categories={['value']}
+          colors={['blue']}
+          valueFormatter={dataFormatter}
+          yAxisWidth={48}
         />
       </div>
     </div>
   );
 }
+
+
--- front/src/components/charts/EmployeeChart.jsx
+++ front/src/components/charts/EmployeeChart.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FunnelChart } from '@tremor/react';
+import { BarChart } from '@tremor/react';
 
 const chartdata = [
   { name: 'Servicio A', value: 89 },
@@ -8,29 +8,33 @@ const chartdata = [
   { name: 'Servicio C', value: 5 },
 ];
 
+const dataFormatter = (number) => Intl.NumberFormat('us').format(number).toString();
+
 export function EmployeeChart() {
-    return (
-        <div className="border border-black rounded-lg p-4 bg-white">
-          <h3 className="text-black text-tremor-default text-tremor-content dark:text-dark-tremor-content text-sm">
-            Mejor Lubricador
-          </h3>
-          <p className="text-black text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content text-sm">
-            5.6%
-          </p>
-          <p className="mt-4 text-black text-tremor-label text-tremor-content dark:text-dark-tremor-content text-sm">
-          Lubricadores únicos en un orden específico que se convirtieron en los últimos 30 días.
-          </p>
-          <div className="flex justify-center">
-            <FunnelChart
-              className="mt-4 w-full max-w-md"
-              data={chartdata}
-              style={{
-                label: { fill: 'black', fontSize: 12, fontWeight: 'bold' },
-                tooltip: { background: 'white', border: '1px solid #ccc', color: 'black', fontSize: 12, fontWeight: 'normal' }
-              }}
-            />
-          </div>
-        </div>
-      );
-    }
+  return (
+    <div className="border border-black rounded-lg p-4 bg-white">
+      <h3 className="text-black text-tremor-default dark:text-dark-tremor-content">
+        Mejor Lubricador
+      </h3>
+      <p className="text-black text-tremor-metric font-semibold dark:text-dark-tremor-content">
+        5.6%
+      </p>
+      <p className="mt-4 text-black text-tremor-label dark:text-dark-tremor-content">
+        Lubricadores únicos en un orden específico que se convirtieron en los últimos 30 días.
+      </p>
+      <div className="flex justify-center">
+        <BarChart
+          className="mt-4 w-full max-w-md"
+          data={chartdata}
+          index="name"
+          categories={['value']}
+          colors={['blue']}
+          valueFormatter={dataFormatter}
+          yAxisWidth={48}
+        />
+      </div>
+    </div>
+  );
+}
+
 
