73ed539714f432bf01dbeff9298a5e718b04202f
ARagustinaramos <agusramos12@gmail.com>
2024-07-17 11:27:29 -0300

    formularios de agregar productos/serivios acomodado

--- front/src/views/new_products_serivice/NewProductsService.jsx
+++ front/src/views/new_products_serivice/NewProductsService.jsx
@@ -3,11 +3,16 @@ import ServiceForm from "../../components/service_form/ServiceForm";
 
 const ProductService = () => {
     return (
-        <div className="floex flex-col space-y-8">
-            <ProductForm/>
-            <ServiceForm/>
+        <div className="flex flex-col md:flex-row md:justify-center md:space-x-8 p-4 bg-gray-100 min-h-screen items-center">
+            <div className="w-full md:w-1/2 lg:w-1/3">
+                <ProductForm />
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3">
+                <ServiceForm />
+            </div>
         </div>
     );
 };
 
 export default ProductService;
+
