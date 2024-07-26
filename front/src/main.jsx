acf969bdafd6af5e3dcc86b6efedc42aa104dd8d
ARagustinaramos <agusramos12@gmail.com>
2024-07-26 13:09:34 -0300

    inventario vista del empleado mostrando productos + pagindo

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store/store.js'
-import { Provider } from 'react-redux'
-import store from './redux/store/store.js'
 import App from './App.jsx'
 import './index.css'
 
