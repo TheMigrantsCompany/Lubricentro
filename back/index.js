1bf211e2e57aa69aad0eda9eb6c43d5a1d4e5bd5
BryanCamelo09 <camelobryan07@gmail.com>
2024-07-30 09:31:41 -0500

    actualización rama develop a mi rama

--- back/index.js
+++ back/index.js
@@ -4,11 +4,7 @@ const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, async () => {
 	try {
-<<<<<<< HEAD
-		await sequelize.sync({ force: true});
-=======
 		await sequelize.sync({ force: false });
->>>>>>> develop
 		console.log(`Server listening on port http://localhost:${PORT}`);
 	} catch (error) {
 		console.error(error);
