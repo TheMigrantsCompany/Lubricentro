9060b137428e8dc1e5c8b3378d670caebd08c7f8
ARagustinaramos <agusramos12@gmail.com>
2024-08-02 10:23:38 -0300

    Filtros por categoria funcionando en Manage Products

--- front/src/redux/actions/actions.js
+++ front/src/redux/actions/actions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { GET_ALL_PRODUCTS, GET_PRODUCTS_ERROR, SEARCH_PRODUCTS } from "./types";
+import { 
+    GET_ALL_PRODUCTS,
+    GET_PRODUCTS_ERROR,
+    SEARCH_PRODUCTS,
+    GET_CATEGORY_BY_ID,
+    GET_PRODUCTS_BY_CATEGORY,
+    GET_ALL_CATEGORIES,  } from "./types";
 
 // Acción para obtener todos los productos
 export const getAllProducts = () => async dispatch => {
@@ -48,4 +54,53 @@ export const searchProducts = (query) => async dispatch => {
           payload: error.message,
       });
   }
+};
+
+// Acción para obtener la categoría por ID
+export const getCategoryById = (categoryId) => async dispatch => {
+    try {
+        const response = await axios.get(`http://localhost:3001/categories/id/${categoryId}`);
+        dispatch({
+            type: GET_CATEGORY_BY_ID,
+            payload: response.data,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_PRODUCTS_ERROR,
+            payload: error.message,
+        });
+        throw error;
+    }
+};
+
+// Acción para obtener productos por categoría
+export const getProductsByCategory = (categoryId) => async dispatch => {
+    try {
+        const response = await axios.get(`http://localhost:3001/products/category/${categoryId}`);
+        dispatch({
+            type: GET_PRODUCTS_BY_CATEGORY,
+            payload: response.data,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_PRODUCTS_ERROR,
+            payload: error.message,
+        });
+    }
+};
+
+// Acción para obtener todas las categorías
+export const getAllCategories = () => async dispatch => {
+    try {
+        const response = await axios.get('http://localhost:3001/categories');
+        dispatch({
+            type: GET_ALL_CATEGORIES,
+            payload: response.data,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_PRODUCTS_ERROR,
+            payload: error.message,
+        });
+    }
 };
--- front/src/redux/reducer/reducer.js
+++ front/src/redux/reducer/reducer.js
@@ -1,26 +1,43 @@
-import { GET_ALL_PRODUCTS, GET_PRODUCTS_ERROR, SEARCH_PRODUCTS } from "../actions/types";
+import { 
+   GET_ALL_PRODUCTS,
+   GET_PRODUCTS_ERROR,
+   SEARCH_PRODUCTS,
+   GET_CATEGORY_BY_ID, 
+   GET_PRODUCTS_BY_CATEGORY,
+   GET_ALL_CATEGORIES, } from "../actions/types";
 
 const initialState = {
     loading: false,
     products: [],
     error: null,
+    category: null,
+    categories: [],
 };
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_ALL_PRODUCTS:
+        case SEARCH_PRODUCTS:
+        case GET_PRODUCTS_BY_CATEGORY:
           return {
             ...state,
             loading: false,
             products: action.payload,
             error: null,
           };
-          case SEARCH_PRODUCTS:
+        case GET_CATEGORY_BY_ID:
+          return {
+            ...state,
+            loading: false,
+            category: action.payload,
+            error: null,
+          };
+          case GET_ALL_CATEGORIES:
             return {
-              ...state,
-              loading: false,
-              products: action.payload,
-              error: null,
+                ...state,
+                loading: false,
+                categories: action.payload,
+                error: null,
             };
         case GET_PRODUCTS_ERROR:
           return {
